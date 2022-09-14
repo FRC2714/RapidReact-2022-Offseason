9f15d05f6d3ea18d6f8ed0b352da2a0f93f00459
KJM312 <irvingywang@gmail.com>
2022-09-13 19:35:56 -0500

    @JasonKlm
    Added lut to hood angle

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -4,48 +4,65 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.*;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import frc.robot.Constants.HoodConstants;
+import frc.robot.util.InterpolatingTreeMap;
 
 public class Hood extends SubsystemBase {
-  private CANSparkMax HoodMotor;
+  private CANSparkMax hoodMotor;
+  private Limelight limelight;
 
   private RelativeEncoder hoodEncoder;
   private SparkMaxPIDController hoodPID;
-
-  private CANSparkMaxLowLevel.MotorType Brushless = CANSparkMaxLowLevel.MotorType.kBrushless;
-  private CANSparkMax.ControlType Position = CANSparkMax.ControlType.kPosition;
 
   private double defaultPosition = 20;
   private double midShotPosition = 70;
   private double targetPosition = 0;
 
+  public InterpolatingTreeMap hoodPosition = new InterpolatingTreeMap();
+
   /** Creates a new Hood. */
   public Hood() {
-    HoodMotor = new CANSparkMax(HoodConstants.kHoodMotorPort, Brushless);
+    hoodMotor = new CANSparkMax(HoodConstants.kHoodMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-    HoodMotor.setSmartCurrentLimit(60);
+    hoodMotor.setSmartCurrentLimit(60);
 
-    hoodEncoder = HoodMotor.getEncoder();
+    hoodEncoder = hoodMotor.getEncoder();
 
-    HoodMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    hoodMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
-    hoodPID = HoodMotor.getPIDController();
+    hoodPID = hoodMotor.getPIDController();
     hoodPID.setP(HoodConstants.kHoodP);
     hoodPID.setI(HoodConstants.kHoodI);
     hoodPID.setD(HoodConstants.kHoodD);
 
     hoodEncoder.setPosition(0);
+    // hoodMotor.setSoftLimit(SoftLimitDirection.kForward, 0)
+    // hoodMotor.setSoftLimit(SoftLimitDirection.kReverse, 0)
+
+    populateMap();
+
   }
 
   public void setTargetPosition(double targetPosition) {
-    this.targetPosition = targetPosition;
-    hoodPID.setReference(targetPosition, Position);
+    hoodPID.setReference(targetPosition, ControlType.kPosition);
   }
 
   public void setMidShot() {
     setTargetPosition(midShotPosition);
+  }
+
+  public void populateMap() {
+    hoodPosition.put(3.0, 200.0);
+    hoodPosition.put(6.0, 450.0);
+    hoodPosition.put(9.0, 600.0);
+    hoodPosition.put(15.0, 750.0); 
   }
 
   public void setDefault() {
@@ -60,9 +77,18 @@ public class Hood extends SubsystemBase {
     return Math.abs(-targetPosition - getPosition()) < HoodConstants.kPositionTolerance;
   }
 
+  public double getHoodPosition() {
+    return limelight.targetVisible()
+        ? hoodPosition.getInterpolated(Units.metersToFeet(limelight.getDistanceToGoal())) : defaultPosition;
+  }
+
+  public void setDynamicPosition() {
+    setTargetPosition(getHoodPosition());
+  }
+
   public void disable() {
-    hoodPID.setReference(defaultPosition, Position);
-    HoodMotor.set(0);
+    hoodPID.setReference(defaultPosition, ControlType.kPosition);
+    hoodMotor.set(0);
   }
 
   @Override
