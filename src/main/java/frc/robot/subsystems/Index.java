c7b9320e385da242d9c56cddb1709cf8768d2fa2
Jason Kim <jason1234lol@gmail.com>
2022-09-17 02:27:06 -0500

    fixed dumbdumb logic
    Co-Authored-By: Ritesh Kanchi <ritesh.kanchi@gmail.com>

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -8,94 +8,84 @@ import frc.robot.Constants.IndexConstants;
 import com.revrobotics.CANSparkMaxLowLevel;
 
 public class Index extends SubsystemBase {
-  private CANSparkMax indexMotor;
-  private CANSparkMax rollerMotor;
-  private DigitalInput indexBeam;
-  private DigitalInput rollerBeam;
+    private CANSparkMax indexMotor;
+    private CANSparkMax rollerMotor;
+    private DigitalInput indexBeam;
+    private DigitalInput rollerBeam;
 
-  public enum IndexState {
-    SHOOTING,
-    INTAKING,
-    EXTAKING,
-    DEFAULT
+    public enum IndexState {
+        SHOOTING,
+        INTAKING,
+        EXTAKING,
+        DEFAULT
     }
 
     private IndexState indexState = IndexState.DEFAULT;
 
-  /** Creates a new Index. */
-  public Index() {
-    indexMotor = new CANSparkMax(IndexConstants.kIndexMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
-    rollerMotor = new CANSparkMax(IndexConstants.kRollerMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
-    indexBeam = new DigitalInput(IndexConstants.kIndexBeamChannel);
-    rollerBeam = new DigitalInput(IndexConstants.kRollerBeamChannel);
-}
-
-  public void setRollerPower(double power){
-    rollerMotor.set(power);
-  }
-
-  public void setIndexPower(double power){
-    indexMotor.set(power);
-  }
-
-  public void moveAll(double power){
-      indexMotor.set(power);
-      rollerMotor.set(power);
-  }
-
-  public void disable() {
-      moveAll(0);
-      setIndexState(IndexState.DEFAULT);
-  }
-
-  public boolean getIndexBreakbeam() {
-
-   if(indexBeam.get()) {
-       return true;
-   }
-   else {
-        return false;
+    /** Creates a new Index. */
+    public Index() {
+        indexMotor = new CANSparkMax(IndexConstants.kIndexMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+        rollerMotor = new CANSparkMax(IndexConstants.kRollerMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+        indexBeam = new DigitalInput(IndexConstants.kIndexBeamChannel);
+        rollerBeam = new DigitalInput(IndexConstants.kRollerBeamChannel);
     }
-  }
 
-  public boolean getRollerBreakbeam() {
-    if(rollerBeam.get()) {
-        return true;
+    public void setRollerPower(double power) {
+        rollerMotor.set(power);
     }
-    else {
-         return false;
+
+    public void setIndexPower(double power) {
+        indexMotor.set(power);
     }
-  }
 
-  public void setIndexState(IndexState indexState) {
-    this.indexState = indexState;
-  }
+    public void moveAll(double power) {
+        indexMotor.set(power);
+        rollerMotor.set(power);
+    }
 
-  public void IndexMotion() {
-      if (indexState == IndexState.SHOOTING) {
-          moveAll(.5);
-      }
-      if (indexState == IndexState.EXTAKING) {
-          moveAll(-.5);
-      }
-      if (indexState == IndexState.INTAKING){
-          if(getIndexBreakbeam() == true) {
-              setIndexPower(0);
-              setRollerPower(.5);
-          }
-          if(getIndexBreakbeam() & getRollerBreakbeam() == true) {
-              moveAll(.5);
-          }
-          else {
-              moveAll(.5);
-          }
-      }
-  }
+    public void disable() {
+        moveAll(0);
+        setIndexState(IndexState.DEFAULT);
+    }
 
+    public boolean getIndexBreakbeam() {
+        return indexBeam.get();
+    }
 
+    public boolean getRollerBreakbeam() {
+        return rollerBeam.get();
+    }
 
-  @Override
-  public void periodic() {
-    
-  }
+    public void setIndexState(IndexState indexState) {
+        this.indexState = indexState;
+    }
+
+    public void IndexMotion() {
+        if (indexState == IndexState.SHOOTING) {
+            moveAll(.5);
+        }
+        if (indexState == IndexState.EXTAKING) {
+            moveAll(-.5);
+        }
+        if (indexState == IndexState.INTAKING) {
+
+            setRollerPower(.5);
+
+            if (getIndexBreakbeam()) {
+                setIndexPower(0);
+
+                if (getRollerBreakbeam()) {
+                    moveAll(.5);
+                }
+                
+            } else {
+                moveAll(.5);
+            }
+        }
+    }
+
+    @Override
+    public void periodic() {
+
+    }
 }
